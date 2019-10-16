83ff8685e277c3e237d0d59f3bd6095c7cee8686
Mateusz Kruk <cienkarenta@gmail.com>
2019-10-16 22:30:01 +0200

    Made basic MVC structure and basic layout

---
+++ src/FitBuddy/Controllers/FitBuddyController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FitBuddy.Models;
+
+namespace FitBuddy.Controllers
+{
+    public class FitBuddyController : Controller
+    {
+        // GET: FitBuddy
+
+        Product produkt1 = new Product()
+        {
+            Id=1,
+            Name="Chlebek Boży",
+            Kcal=1337,
+            Carbo=400,
+            Whey=100,
+            Fat=10
+
+
+        };
+
+        public ActionResult Index()
+        {
+            return View(produkt1);
+        }
+
+
+    }
+}
---
+++ src/FitBuddy/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FitBuddy.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Kcal { get; set; }
+        public int Carbo { get; set; }
+        public int Whey { get; set; }
+        public int Fat { get; set; }
+
+        public string DisplayProduct
+        {
+            get
+            {
+                return Id + " " + Name + " " + Kcal + "kcal  " + "Macro:" + "Carb " + Carbo + " Protein " + Whey + " Fats " + Fat;
+            }
+        }
+    }
+}
