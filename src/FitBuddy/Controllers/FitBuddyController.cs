b189dc05fdf04c6576b6b65b0ae3ff0af7449cb6
Mateusz Kruk <cienkarenta@gmail.com>
2019-10-30 00:15:08 +0100

    Made Simple login-register system

--- src/FitBuddy/Controllers/FitBuddyController.cs
+++ src/FitBuddy/Controllers/FitBuddyController.cs
@@ -18,9 +18,12 @@ namespace FitBuddy.Controllers
         // GET: FitBuddy
 
         public string existing;
+        public static Int32 helper = 1;
+        private static bool status = false;
+        private static int register = 0;
 
 
-        public ActionResult Indexx()
+        public ActionResult Index()
         {
             return View();
         }
@@ -33,24 +36,32 @@ namespace FitBuddy.Controllers
         [HttpPost]
         public ActionResult Add(Product product)
         {
-            
-            if (!ModelState.IsValid)
+           if(!status)
             {
-                ViewBag.existing = "Fill up every field!";
-                return View(); ;
-            }
-            if(db.Products.Find(product.Name)!=null)
-            {
-                ViewBag.existing = "This item is already in database!";
+                ViewBag.existing = "You have to be looged in to add product!";
                 return View();
             }
-            else
+           else
             {
-                ViewBag.existing = "Item succesfully added!";
-                db.Products.Add(product);
-                db.SaveChanges();
-                return View("Add");
+                if (!ModelState.IsValid)
+                {
+                    ViewBag.existing = "Fill up every field!";
+                    return View(); ;
+                }
+                if (db.Products.Find(product.Name) != null)
+                {
+                    ViewBag.existing = "This item is already in database!";
+                    return View();
+                }
+                else
+                {
+                    ViewBag.existing = "Item succesfully added!";
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                    return View();
+                }
             }
+            
 
          
 
@@ -74,26 +85,109 @@ namespace FitBuddy.Controllers
 
             if(prod == null)
             {
-                Product notf = new Product()
-                {
-                Name = "Not Found",
-                Kcal = 0,
-                Protein=0,
-                Carb=0,
-                Fats=0
+                ViewBag.existing = "This product is not in our database!";
+
+                //Product notf = new Product()
+                //{
+
+                //Name = "    ",
+                //Kcal = 0,
+                //Protein=0,
+                //Carb=0,
+                //Fats=0
 
                 
-                };
+                //};
 
-                return View(notf);
+                return View();
 
             }
 
             return View(prod);
           
-            
-            
+        }
 
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public UsersContext dbu = new UsersContext();
+
+        [HttpPost]
+        public ActionResult Login(string Nick, string pass)
+        {
+
+            if (Nick == null || pass==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            User us = dbu.Users.Find(Nick);
+
+            if(dbu.Users.Find(Nick) == null)
+            {
+                ViewBag.existing = "Fail";
+                return View();
+            }
+
+            if (!(pass == us.Pass))
+            {
+                ViewBag.existing = "Fail";
+                return View();
+            }
+
+            if(status)
+            {
+                ViewBag.existing = "You are already logged in!";
+                return View();
+            }
+
+            if (pass == us.Pass)
+            {
+                status = true;
+                ViewBag.existing = "Logged!";
+                return View();
+            }
+           
+
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.existing = "Fill up every field!";
+                return View(); ;
+            }
+            if (dbu.Users.Find(user.Nick) != null)
+            {
+                ViewBag.existing = "There is user with that nickname!";
+                return View();
+            }
+            else
+            {
+                ViewBag.existing = "Feel free to log in!";
+                List<User> help = dbu.Users.ToList();
+                user.IDu = help.Count;
+                dbu.Users.Add(user);
+                dbu.SaveChanges();
+                return View();
+
+            }
+
+            
+                
+
+
+            return View();
         }
 
     }
---
+++ src/FitBuddy/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FitBuddy.Models
+{
+    public class User
+    {
+        public int IDu{ get; set; }
+        [Key]
+        [Required(ErrorMessage = " ")]
+        public string Nick{ get; set; }
+        [Required(ErrorMessage = " ")]
+        public string Pass{ get; set; }
+       
+    }
+}
